9c77c0b43b49e4b034bb10e5307fc4a103f71127
AmChaoSun <leosunchao@gmail.com>
2019-02-03 02:04:36 +1100

    finish chapter one

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OO_Design_Patterns.SimpleFactory;
 
 namespace OO_Design_Patterns
 {
@@ -7,6 +8,25 @@ namespace OO_Design_Patterns
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+
+            //Chapter 1 SimpleFactory
+            Console.WriteLine("Input operator");
+            string op = Console.ReadLine();
+            var operation = OperationFactory.CreateOperation(op);
+
+            Console.WriteLine("Input NumA:");
+            string stringNumA = Console.ReadLine();
+            Console.WriteLine("Input NumB:");
+            string stringNumB = Console.ReadLine();
+            double numA = Convert.ToDouble(stringNumA);
+            double numB = Convert.ToDouble(stringNumB);
+
+            operation.NumA = numA;
+            operation.NumB = numB;
+            var result = operation.GetResult();
+            Console.WriteLine("Result is " + result);
         }
     }
 }
